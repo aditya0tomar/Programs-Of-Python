13d52de695c16c1a6690e20d9ceeb7e8891812b4
aditya0tomar <adityasinghtomar0432@gmail.com>
2025-05-28 19:53:47 +0530

    28-05-25

---
+++ repeat_string.py
@@ -0,0 +1,9 @@
+def double_char ( input_str ):
+    doubled_str = ""
+
+    for char in input_str:
+           doubled_str +=char*2
+    return doubled_str
+print(double_char("string"))
+print(double_char("doublee-2"))
+print(double_char("adi..55"))
